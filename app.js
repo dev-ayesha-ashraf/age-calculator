1d489326f6f3268aa7a23700625a0ed30f6eaaa4
dev-ayesha-ashraf <ayeshaashraf0091@gmail.com>
2023-05-27 20:38:12 +0500

    age-calculator-updated

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+
+
 function calculateAge() {
-    var currentYear = 2023;
-      var birthYear = parseInt(document.getElementById('birthYear').value);
-      var age = currentYear - birthYear;
-      var adjustedAge = age - 1;
-      var gender = prompt("Please enter your gender:");
-        document.getElementById('ageResult').textContent =
-          "You may be " + adjustedAge + " or " + age + " years old " + gender;
-    }
+  var birthdateInput = document.getElementById('birthdate');
+  var birthdate = new Date(birthdateInput.value);
+
+  var today = new Date();
+  var ageInYears = today.getFullYear() - birthdate.getFullYear();
+  var ageInMonths = today.getMonth() - birthdate.getMonth();
+  var ageInDays = today.getDate() - birthdate.getDate();
+
+  if (ageInDays < 0) {
+    ageInMonths--;
+    var daysInPreviousMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    ageInDays += daysInPreviousMonth;
+  }
+
+  if (ageInMonths < 0) {
+    ageInYears--;
+    ageInMonths += 12;
+  }
+
+  var resultElement = document.getElementById('result');
+  resultElement.textContent = "Your age is " + ageInYears + " years, " + ageInMonths + " months, and " + ageInDays + " days.";
+}
+
+
